ecc10b9ba712278058d11647491c06460e752813
Meihan <63854535+Mei8n@users.noreply.github.com>
2026-03-26 21:07:23 +0900

    Rendererのリネーム

--- src/main/java/jp/me1han/sam/render/RendererStopAnnouncer.java
+++ src/main/java/jp/me1han/sam/render/RendererStopAnnouncer.java
@@ -3,7 +3,7 @@ package jp.me1han.sam.render;
 import jp.me1han.sam.client.SAMRendererBase;
 import net.minecraft.tileentity.TileEntity;
 
-public class StartAnnouncerRenderer extends SAMRendererBase {
+public class RendererStopAnnouncer extends SAMRendererBase {
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks) {
         this.renderCustomBeam(te, x, y, z, partialTicks);
--- src/main/java/jp/me1han/sam/render/RendererTrainTypeSelector.java
+++ src/main/java/jp/me1han/sam/render/RendererTrainTypeSelector.java
@@ -3,9 +3,11 @@ package jp.me1han.sam.render;
 import jp.me1han.sam.client.SAMRendererBase;
 import net.minecraft.tileentity.TileEntity;
 
-public class StopAnnouncerRenderer extends SAMRendererBase {
+public class RendererTrainTypeSelector extends SAMRendererBase {
+
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks) {
+        // 共通のビーム描画メソッドを呼び出し
         this.renderCustomBeam(te, x, y, z, partialTicks);
     }
 }
